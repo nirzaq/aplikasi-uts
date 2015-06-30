9fdcf0c05a30876764f1751e64b798208515822b
nirzaq <synyster.arief@gmail.com>
2015-06-30 19:18:09 +0800

    Domain Object

---
+++ src/main/java/id/arief/belajar/aplikasi/crud/Mahasiswa.java
@@ -0,0 +1,78 @@
+package id.arief.belajar.aplikasi.crud;
+
+import java.util.Date;
+
+public class Mahasiswa {
+
+private Integer id;
+private String nim;
+private String nama;
+private String tempatLahir;
+private Date tanggalLahir;
+private JenisKelamin jenisKelamin;
+private String alamat;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getTempatLahir() {
+        return tempatLahir;
+    }
+
+    public void setTempatLahir(String tempatLahir) {
+        this.tempatLahir = tempatLahir;
+    }
+
+    public Date getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(Date tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+
+    public JenisKelamin getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setJenisKelamin(JenisKelamin jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+
+
+
+
+
+
+
+}
