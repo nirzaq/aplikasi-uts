5030ce002411b30c667416f20956d20a2d5895d2
nirzaq <synyster.arief@gmail.com>
2015-06-30 19:12:21 +0800

    File Main App

---
+++ src/main/java/id/arief/belajar/App.java
@@ -0,0 +1,13 @@
+package id.arief.belajar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
